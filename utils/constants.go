ef94ca09088ea4ed831c6af5b7fb5f3cf8b47f34
porkytheblack <porkytheblack@gmail.com>
2023-03-16 15:43:41 +0300

    chore: updated send url

--- utils/constants.go
+++ utils/constants.go
@@ -1,6 +1,13 @@
 package utils
 
-const AFRICAS_TALKING_SEND_URL = "https://api.sandbox.africastalking.com/version1/messaging"
+import "os"
+
+var AFRICAS_TALKING_SEND_URL = (func (env string)string {
+	if env == "production" {
+		return "https://api.africastalking.com/version1/messaging"
+	}
+	return "https://api.sandbox.africastalking.com/version1/messaging"
+})(os.Getenv("APP_ENV"))
 
 const ACTIVATION_CODE = "777"
 const DEACTIVATION_CODE = "444"
